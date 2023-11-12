5d672e20cacc5bd8115778218f1e4621b84efb66
YehorGorishnii <131007854+YehorGorishnii@users.noreply.github.com>
2023-11-12 16:25:52 +0300

    hw-4

---
+++ task-1.js
@@ -0,0 +1,15 @@
+function isEnoughCapacity(products, containerSize) {
+  let values = Object.values(products);
+  let sum = 0;
+  for (const value of values) {
+    sum += value;
+  }
+  return sum <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
